840658091fb00dcaa645c9edc8d9d3b14de52da8
Oluchukwu Anakor <anakorafav@gmail.com>
2026-09-08 11:00:01 +0000

    feat: configure django admin for department and employee

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Department, Employee
 
 # Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at']
+    search_fields = ['name']
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'department', 'is_active']
+    search_fields = ['first_name', 'last_name', 'email']
+    list_filter = ['department', 'is_active']
+
+
